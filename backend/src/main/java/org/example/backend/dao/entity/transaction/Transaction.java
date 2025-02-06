7b20170ff9d9cc8827a742191ba0579e0bb4c27a
Maks Garczynski <rgarczynski66@gmail.com>
2025-02-06 16:08:12 +0100

    Created Transaction and TransactionProduct super classes

---
+++ backend/src/main/java/org/example/backend/dao/entity/transaction/Transaction.java
@@ -0,0 +1,29 @@
+package org.example.backend.dao.entity.transaction;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@MappedSuperclass
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public abstract class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private Date transactionDate;
+
+    public Transaction(Date transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+}
---
+++ backend/src/main/java/org/example/backend/dao/entity/transaction/TransactionProduct.java
@@ -0,0 +1,32 @@
+package org.example.backend.dao.entity.transaction;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@MappedSuperclass
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public abstract class TransactionProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private Long quantity;
+
+    @Column(nullable = false)
+    private Double pricePerUnit;
+
+    public TransactionProduct(Long quantity, Double pricePerUnit) {
+        this.quantity = quantity;
+        this.pricePerUnit = pricePerUnit;
+    }
+}
