0b15cb8bc036317ee1c5ff3b539391d6a4a0a883
Maks Garczynski <rgarczynski66@gmail.com>
2025-02-11 14:13:58 +0100

    Made small change in UserDataService method saveNewRole

--- backend/src/main/java/org/example/backend/dao/service/user/UserDataService.java
+++ backend/src/main/java/org/example/backend/dao/service/user/UserDataService.java
@@ -164,6 +164,10 @@ public class UserDataService {
             throw new RoleNotSavedException(e.getMessage());
         }
 
+        privileges.forEach(privilege -> {
+            privilege.getRoles().add(roleSaved);
+        });
+
         return roleSaved;
     }
 
