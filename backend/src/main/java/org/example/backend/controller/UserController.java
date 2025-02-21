2226dded1a3e0b2f6ee9c54a94433cc762c1bdec
Maks Garczynski <rgarczynski66@gmail.com>
2025-02-21 20:19:49 +0100

    Created getUserProfileImage method in UserController class

--- backend/src/main/java/org/example/backend/controller/UserController.java
+++ backend/src/main/java/org/example/backend/controller/UserController.java
@@ -102,6 +102,22 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(foundUser);
     }
 
+    @GetMapping("/user/profile-image")
+    public ResponseEntity<UserIdAndImage> getUserProfileImage(@RequestBody UUID userId) {
+
+        UserIdAndImage userIdAndImage;
+
+        try{
+            userIdAndImage = userDataService.getUserIdAndImageById(userId);
+        } catch (BadArgumentException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch (UserNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(userIdAndImage);
+    }
+
     @DeleteMapping("/delete-user-by-id/{id}")
     public ResponseEntity deleteUserById(@PathVariable("id") UUID id) {
 
