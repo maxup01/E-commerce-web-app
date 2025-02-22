cda9475e0e18ef3e04d9839d642e8e824d174e19
Maks Garczynski <rgarczynski66@gmail.com>
2025-02-22 22:31:37 +0100

    Created OrderTransactionController class with getOrderTransactionById method

---
+++ backend/src/main/java/org/example/backend/controller/OrderTransactionController.java
@@ -0,0 +1,41 @@
+package org.example.backend.controller;
+
+import org.example.backend.dao.service.OrderTransactionService;
+import org.example.backend.exception.global.BadArgumentException;
+import org.example.backend.exception.transaction.OrderTransactionNotFoundException;
+import org.example.backend.model.OrderTransactionModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class OrderTransactionController {
+
+    private final OrderTransactionService orderTransactionService;
+
+    @Autowired
+    public OrderTransactionController(OrderTransactionService orderTransactionService) {
+        this.orderTransactionService = orderTransactionService;
+    }
+
+    @GetMapping("/order")
+    public ResponseEntity<OrderTransactionModel> getOrderTransactionById(@RequestBody UUID orderId) {
+
+        OrderTransactionModel orderTransactionModel;
+
+        try{
+            orderTransactionModel = orderTransactionService.getOrderTransactionById(orderId);
+        } catch (BadArgumentException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch (OrderTransactionNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(orderTransactionModel);
+    }
+}
