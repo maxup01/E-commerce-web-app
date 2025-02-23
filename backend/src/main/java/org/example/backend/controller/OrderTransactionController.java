5092927fc29e7ce4d3e52bd6e3f67adabf5a087b
Maks Garczynski <rgarczynski66@gmail.com>
2025-02-23 17:32:20 +0100

    I have added getOrderTransactionsByPaymentMethodNameAndDeliveryProviderName and getOrderTransactionsByPaymentMethodNameAndUserEmail methods in OrderTransactionService class

--- backend/src/main/java/org/example/backend/controller/OrderTransactionController.java
+++ backend/src/main/java/org/example/backend/controller/OrderTransactionController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -38,6 +39,14 @@ public class OrderTransactionController {
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(orderTransactionModel);
+    }
+
+    @GetMapping("/orders")
+    public ResponseEntity<List<OrderTransactionModel>> getOrderTransactions(
+            @RequestBody TimePeriodModel timePeriodModel) {
+
+        //TODO make this function body to looks like body in getProductsByProductSearchModel in ProductController
+        return null;
     }
 
     @GetMapping("/orders/count-by-time-period")
