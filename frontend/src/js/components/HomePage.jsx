166e22c601d18ba95680bbf41f1bc5995b4411bc
Maks Garczynski <rgarczynski66@gmail.com>
2025-03-01 16:47:27 +0100

    Added ProductSearchBar.jsx in component directory but not completed

--- frontend/src/js/components/HomePage.jsx
+++ frontend/src/js/components/HomePage.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 function HomePage() {
 
+    const divStyle = {
+        width: '100%',
+        height: '100%',
+    }
+
     return (
-        <div>
+        <div style={divStyle}>
             TODO
         </div>
     )
---
+++ frontend/src/js/components/ProductSearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductSearchBar() {
+    return (
+        <div className="search-bar">
+            TODO
+        </div>
+    );
+}
+
+export default ProductSearchBar;
