83b569d1c99f9b6be37423caf6a73acdc2890ee2
Maks Garczynski <rgarczynski66@gmail.com>
2025-03-02 20:45:35 +0100

    Created ProductSearchBar and added two dependencies in backend module spring boot actuator and axios in frontend module

--- frontend/src/js/components/ProductSearchBar.jsx
+++ frontend/src/js/components/ProductSearchBar.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 
-function ProductSearchBar() {
+
+function ProductSearchBar(props) {
+
+    const [types, setTypes] = React.useState([]);
+
+    useEffect(() => {
+        axios
+            .get('http://127.0.0.1:8080/all-product-types')
+            .then(res => {
+                setTypes(res.data);
+            });
+    });
+
+    const divStyle = {
+        width: props.width,
+        height: props.height
+    }
+
     return (
-        <div className="search-bar">
-            TODO
+        <div style={divStyle}>
+            <select id={"type"} className={"bg-cyan-50 w-1/5"}>
+                {types.map(type => (
+                    <option key={type} value={type}>{type}</option>
+                ))}
+            </select>
+            <input type={"text"} className={"bg-cyan-50 w-4/5 placeholder-gray-700"} placeholder={"Search"} />
         </div>
     );
 }
